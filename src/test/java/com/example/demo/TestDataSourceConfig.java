8111ddcb5fa2095a6888ddc57c4a587dbcb0b121
brev <phan.brian07@gmail.com>
2025-07-27 15:15:02 -0400

    init

---
+++ src/test/java/com/example/demo/TestDataSourceConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@TestConfiguration
+public class TestDataSourceConfig {
+
+    @Bean
+    @Primary
+    public DataSource primaryDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("db1")
+                .addScript("classpath:sql/db1-data.sql")
+                .build();
+    }
+
+    @Bean
+    public DataSource secondaryDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("db2")
+                .addScript("classpath:sql/db2-data.sql")
+                .build();
+    }
+}
